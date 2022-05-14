04afa8b0a444eeda7a61018b9cea0c42aa7e10c1
siri-ayya <nandawathiganithage@gmail.com>
2022-05-14 18:57:53 +0530

    initial commit

---
+++ app/routes/join.tsx
@@ -0,0 +1,183 @@
+import type {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import * as React from "react";
+
+import { getUserId, createUserSession } from "~/session.server";
+
+import { createUser, getUserByUsername } from "~/models/user.server";
+import { safeRedirect, validateEmail } from "~/utils";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (userId) return redirect("/");
+  return json({});
+};
+
+interface ActionData {
+  errors: {
+    username?: string;
+    password?: string;
+  };
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const username = formData.get("username");
+  const password = formData.get("password");
+  const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
+
+  // if (!validateEmail(email)) {
+  //   return json<ActionData>(
+  //     { errors: { email: "Email is invalid" } },
+  //     { status: 400 }
+  //   );
+  // }
+  if (typeof username !== "string") {
+    return json<ActionData>(
+      { errors: { username: "Password is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (typeof password !== "string") {
+    return json<ActionData>(
+      { errors: { password: "Password is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (password.length < 6) {
+    return json<ActionData>(
+      { errors: { password: "Password is too short" } },
+      { status: 400 }
+    );
+  }
+
+  const existingUser = await getUserByUsername(username);
+  if (existingUser) {
+    return json<ActionData>(
+      { errors: { username: "A user already exists with this email" } },
+      { status: 400 }
+    );
+  }
+
+  const user = await createUser(username, password);
+
+  return createUserSession({
+    request,
+    userId: user.id,
+    remember: false,
+    redirectTo,
+  });
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Sign Up",
+  };
+};
+
+export default function Join() {
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") ?? undefined;
+  const actionData = useActionData() as ActionData;
+  const usernameRef = React.useRef<HTMLInputElement>(null);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (actionData?.errors?.username) {
+      usernameRef.current?.focus();
+    } else if (actionData?.errors?.password) {
+      passwordRef.current?.focus();
+    }
+  }, [actionData]);
+
+  return (
+    <div className="flex min-h-full flex-col justify-center">
+      <div className="mx-auto w-full max-w-md px-8">
+        <Form method="post" className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <div className="mt-1">
+              <input
+                ref={usernameRef}
+                id="username"
+                required
+                autoFocus={true}
+                name="username"
+                type="text"
+                autoComplete="username"
+                aria-invalid={actionData?.errors?.username ? true : undefined}
+                aria-describedby="email-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.username && (
+                <div className="pt-1 text-red-700" id="email-error">
+                  {actionData.errors.username}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                id="password"
+                ref={passwordRef}
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                aria-invalid={actionData?.errors?.password ? true : undefined}
+                aria-describedby="password-error"
+                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              />
+              {actionData?.errors?.password && (
+                <div className="pt-1 text-red-700" id="password-error">
+                  {actionData.errors.password}
+                </div>
+              )}
+            </div>
+          </div>
+
+          <input type="hidden" name="redirectTo" value={redirectTo} />
+          <button
+            type="submit"
+            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          >
+            Create Account
+          </button>
+          <div className="flex items-center justify-center">
+            <div className="text-center text-sm text-gray-500">
+              Already have an account?{" "}
+              <Link
+                className="text-blue-500 underline"
+                to={{
+                  pathname: "/login",
+                  search: searchParams.toString(),
+                }}
+              >
+                Log in
+              </Link>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
